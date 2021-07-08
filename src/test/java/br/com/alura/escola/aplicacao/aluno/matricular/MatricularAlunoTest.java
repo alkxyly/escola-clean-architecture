5133180ccb3ae241c7522c6bd01e3dc335312d4b
alkxyly <alkxyly@hotmail.com>
2021-07-07 21:41:10 -0300

    06.05 Testando o Use Case

---
+++ src/test/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.escola.aplicacao.aluno.matricular;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
+import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+
+class MatricularAlunoTest {
+
+	@Test
+	void AlunoDeveriaSerPersistido() {
+		RepositorioDeAlunos repositorio = new RepositorioDeAlunosEmMemoria();
+		
+		MatricularAluno useCase = new MatricularAluno(repositorio);
+		
+		MatricularAlunoDTO dados = new MatricularAlunoDTO("Fulano","123.123.456-89", "fulano@gmail.com");
+		useCase.executa(dados);
+		
+		Aluno encontrado = repositorio.buscarPorCpf(new CPF("123.123.456-89"));
+		
+		assertEquals("Fulano", encontrado.getNome());
+		assertEquals("123.123.456-89", encontrado.getCpf());
+		
+	}
+
+}
